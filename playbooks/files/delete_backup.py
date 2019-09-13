2db5814fe52b6ad2f37fb3174e02e742d60179e8
Artem Goncharov <Artem.goncharov@gmail.com>
2019-09-13 18:54:06 +0200

    fallback if backup still restoring

--- playbooks/files/delete_backup.py
+++ playbooks/files/delete_backup.py
@@ -12,13 +12,13 @@ for backup in conn.block_storage.backups():
         if backup_found:
             print('Backup with this name was already found, potentially'
                   'multiple')
-        if backup.status == 'creating':
+        if backup.status == 'creating' or backup.status == 'restoring':
             for count in openstack.utils.iterate_timeout(
-                    timeout=300, message='Wait for backup status failed',
+                    timeout=600, message='Wait for backup status failed',
                     wait=5):
                 backup = conn.block_storage.get_backup(backup.id)
                 if backup.status != 'creating':
                     break
         backup_found = True
         conn.block_storage.delete_backup(backup.id)
-        conn.block_storage.wait_for_delete(backup, interval=2, wait=300)
+        conn.block_storage.wait_for_delete(backup, interval=2, wait=600)
