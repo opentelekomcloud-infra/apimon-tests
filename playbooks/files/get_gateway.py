8e22d5591aac235af6515691b0fbcbf86d5afd6b
T. Schreiber <tino.schreiber@t-systems.com>
2019-12-13 13:28:53 +0000

    correct get_gateway.py

--- playbooks/files/get_gateway.py
+++ playbooks/files/get_gateway.py
@@ -1,17 +1,15 @@
 #!/usr/bin/env python3
 
 import openstack
-from otcextensions import sdk
+import sys
 
-openstack.enable_logging(True, http_debug=True)
+openstack.enable_logging(True)
 
 # An 'otc' is a cloud connection with name 'otc' configured in the clouds.yaml
 conn = openstack.connect()
 
-# Register OTC Extensions
-#sdk.register_otc_extensions(conn)
 
-id = 'c545ccdf-05ff-4157-a1e2-cd6b79863827'
+id = sys.argv[1]
 
 gateway = conn.nat.get_gateway(id)
 print(gateway)
