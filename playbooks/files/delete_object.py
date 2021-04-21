43b7a1294f71d475f07dfd9900467095f2f2bc41
Kristian Kucerak <Kristian.Kucerak@t-systems.com>
2021-04-21 11:36:21 +0000

    obs basic scenario lifecycle

---
+++ playbooks/files/delete_object.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+import openstack
+import sys
+
+conn = openstack.connect()
+#openstack.enable_logging(debug=True, http_debug=True)
+
+container_name = sys.argv[1]
+object_name = sys.argv[2]
+
+#delete object
+conn.obs.delete_object(object_name, container=container_name, ignore_missing=True)
+
