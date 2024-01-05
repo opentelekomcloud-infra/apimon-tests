26840603a2e08292051296e54667920c9c76ea99
Polina Gubina <33940358+Polina-Gubina@users.noreply.github.com>
2024-01-05 16:10:40 +0100

    Fix scenario9_obs (#64)
    Fix scenario9_obs
    
    Reviewed-by: Anton Sidelnikov

--- playbooks/files/create_object.py
+++ playbooks/files/create_object.py
@@ -2,12 +2,16 @@
 
 import openstack
 import sys
+from otcextensions import sdk
 
+openstack.enable_logging(True)
 conn = openstack.connect()
+sdk.register_otc_extensions(conn)
+
 # openstack.enable_logging(debug=True, http_debug=True)
 
 container_name = sys.argv[1]
 object_name = sys.argv[2]
 
 # create object
-conn.obs.create_object(container_name, name=object_name)
+conn.obs.create_object(container=container_name, name=object_name)
--- playbooks/files/get_object.py
+++ playbooks/files/get_object.py
@@ -2,8 +2,11 @@
 
 import openstack
 import sys
+from otcextensions import sdk
 
+openstack.enable_logging(True)
 conn = openstack.connect()
+sdk.register_otc_extensions(conn)
 # openstack.enable_logging(debug=True, http_debug=True)
 
 container_name = sys.argv[1]
