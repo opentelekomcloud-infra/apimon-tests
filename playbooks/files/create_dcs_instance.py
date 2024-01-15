07e9f2391ef056e1236d343fe271b8d783179e47
Daniela Ebert <d.ebert@t-systems.com>
2024-01-15 12:31:50 +0000

    Use generated PW for DCS instance; dynamically fetch the AZ

--- playbooks/files/create_dcs_instance.py
+++ playbooks/files/create_dcs_instance.py
@@ -9,19 +9,25 @@ instance_name = sys.argv[1]
 vpc_id = sys.argv[2]
 subnet_id = sys.argv[3]
 sg_id = sys.argv[4]
+inst_pw = sys.argv[5]
+az_name = sys.argv[6]
 
-instance = conn.dcs.create_instance(
-    name=instance_name,
-    available_zones=["eu-de-03"],
-    capacity=2,
-    engine="Redis",
-    engine_version="3.0",
-    maintain_begin="02:00:00",
-    maintain_end="06:00:00",
-    password="Password.123##",
-    product_id="OTC_DCS_SINGLE",
-    resource_spec_code="dcs.single_node",
-    security_group_id=sg_id,
-    subnet_id=subnet_id,
-    vpc_id=vpc_id
-)
+attrs = {
+    "name": instance_name,
+    "engine": "Redis",
+    "capacity": 2,
+    "resource_spec_code": "dcs.single_node",
+    "engine_version": "3.0",
+    "vpc_id": vpc_id,
+    "product_id": "OTC_DCS_SINGLE",
+    "password": inst_pw,
+    "maintain_begin": "02:00:00",
+    "maintain_end": "06:00:00",
+    "available_zones": [
+        az_name
+    ],
+    "subnet_id": subnet_id,
+    "security_group_id": sg_id
+}
+
+instance = conn.dcs.create_instance(**attrs)
