1afd546e329db1f2989efc890ba3f8a5a1e6ad88
Daniela Ebert <d.ebert@t-systems.com>
2024-04-23 10:23:49 +0000

    add file dms_list_products.py

---
+++ playbooks/files/dms_list_products.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+import openstack
+import sys
+
+conn = openstack.connect(cloud='otc')
+
+spec = sys.argv[1]
+for raw in conn.dms.products():
+    if raw.spec_code == spec:
+       print(raw.storage)
+       print(raw.product_id)
+       print(raw.io[0].get("storage_spec_code"))
+       break
+
