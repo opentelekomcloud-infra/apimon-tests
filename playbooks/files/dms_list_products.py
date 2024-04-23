ae74af07f2139aa9cc928dd581f6cabd322c55bd
Daniela Ebert <d.ebert@t-systems.com>
2024-04-23 10:44:01 +0000

    fix E111 indentation is not a multiple of 4

--- playbooks/files/dms_list_products.py
+++ playbooks/files/dms_list_products.py
@@ -8,8 +8,8 @@ conn = openstack.connect(cloud='otc')
 spec = sys.argv[1]
 for raw in conn.dms.products():
     if raw.spec_code == spec:
-       print(raw.storage)
-       print(raw.product_id)
-       print(raw.io[0].get("storage_spec_code"))
-       break
+        print(raw.storage)
+        print(raw.product_id)
+        print(raw.io[0].get("storage_spec_code"))
+        break
 
