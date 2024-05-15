881ddde27347e5a0b66d71f30b7f51437b6cf3a7
danielaebert <d.ebert@t-systems.com>
2024-05-15 11:14:06 +0200

    Add scenario45 - DMS Kafka instance (#78)
    Add scenario45 - DMS Kafka instance
    
    Create, List, Update, Delete DMS Kafka instance.
    
    Reviewed-by: Vladimir Hasko <vladimirhasko@gmail.com>
    Reviewed-by: danielaebert
    Reviewed-by: Anton Sidelnikov

---
+++ playbooks/files/dms_list_products.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+import openstack
+import sys
+
+conn = openstack.connect()
+
+spec = sys.argv[1]
+for raw in conn.dms.products():
+    if raw.spec_code == spec:
+        print(raw.storage)
+        print(raw.product_id)
+        print(raw.io[0].get("storage_spec_code"))
+        break
