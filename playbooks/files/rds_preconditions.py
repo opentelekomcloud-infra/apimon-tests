0adec1948e6a0a1a7fc87eb67a2982c737bca243
lboka <ladislav.boka@t-systems.com>
2024-10-01 10:31:54 +0000

    FIX RDS scenario with region and storage query

---
+++ playbooks/files/rds_preconditions.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python3
+import sys
+import openstack
+import json
+
+def find_latest_version(datastores):
+    max_version = max(datastores, key=lambda x: float(x['name']))
+    return max_version['name']
+
+
+if __name__ == "__main__":
+    try:
+        if len(sys.argv) != 4:
+            print(json.dumps({"error": "Usage: check_rds_flavor.py <flavor_code>"}))
+            sys.exit(1)
+
+        # Replace placeholders with actual values
+        database_name = sys.argv[1]
+        db_versions = json.loads(sys.argv[2])
+        spec_code = sys.argv[3]
+
+        latest_version = find_latest_version(db_versions)
+
+        # Establish a connection using your OpenStack profile
+        conn = openstack.connect()
+
+        # Determine the service type and interface
+        service_type = 'rdsv3'  # Adjust this to match your service (e.g., 'rds', 'trove')
+        interface = 'public'       # Use 'internal' or 'admin' if necessary
+
+        # Retrieve the service endpoint
+        endpoint = conn.session.get_endpoint(service_type=service_type, interface=interface)
+
+        # Retrieve the project ID
+        project_id = conn.current_project_id
+
+        # Retrieve region name
+        region_name = conn.config.region_name
+
+        # Construct the URL
+        url1 = f"{endpoint}/storage-type/{database_name}?version_name={latest_version}"
+
+        # Make the GET request using the authenticated session
+        response = conn.session.get(url1)
+        data1 = response.json()
+
+        # Construct the URL
+        url2 = f"{endpoint}/flavors/{database_name}?spec_code={spec_code}&version_name={latest_version}"
+
+        # Make the GET request using the authenticated session
+        response = conn.session.get(url2)
+        data2 = response.json()
+
+        # Extract the group_type from data2
+        group_type = data2['flavors'][0]['group_type']
+
+        # Initialize storage_type variable
+        storage_type = None
+
+        # Iterate over storage types in data1
+        for storage in data1['storage_type']:
+            if group_type in storage['support_compute_group_type']:
+                storage_type = storage['name']
+                if storage_type == "COMMON":
+                    continue
+                break  # Exit the loop after finding the first match
+
+        # Output the result as JSON
+        print(json.dumps({'region': region_name, 'storage_type': storage_type, 'latest_version':latest_version}))
+
+    except Exception as e:
+        print(f"Error: {str(e)}")
+
