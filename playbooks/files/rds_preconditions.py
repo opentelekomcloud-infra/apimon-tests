908f10e7bd90556a6d6e218f70543ee873b0a23d
lboka <ladislav.boka@t-systems.com>
2024-10-25 20:53:59 +0000

    removing blank spaces

--- playbooks/files/rds_preconditions.py
+++ playbooks/files/rds_preconditions.py
@@ -9,15 +9,16 @@ def find_latest_version(datastores):
     max_version = max(datastores, key=lambda x: float(x["name"]))
     return max_version["name"]
 
+
 def find_smallest_flavor(flavors, az):
     # Initialize variables to track the smallest flavor
     smallest_flavor = None
     smallest_vcpus = float('inf')
-    
+
     for flavor in flavors:
         vcpus = int(flavor['vcpus'])
         az_status = flavor.get('az_status', {})
-        
+
         # Check if the flavor has the desired az
         if az_status.get(az) == "normal":
             # Compare the vcpus to find the smallest
@@ -25,7 +26,7 @@ def find_smallest_flavor(flavors, az):
                 smallest_vcpus = vcpus
                 smallest_flavor = flavor['spec_code']
                 group_type = flavor['group_type']
-    
+
     return smallest_flavor, group_type
 
 
@@ -52,7 +53,7 @@ if __name__ == "__main__":
         region_name = conn.config.region_name
 
         storage_types = list(conn.rdsv3.storage_types(
-            datastore_name=database_name, 
+            datastore_name=database_name,
             version_name=latest_version
             ))
 
@@ -66,7 +67,6 @@ if __name__ == "__main__":
                 if storage_type == "COMMON":
                     continue
                 break  # Exit the loop after finding the first match
-
 
         # Output the result as JSON
         print(
