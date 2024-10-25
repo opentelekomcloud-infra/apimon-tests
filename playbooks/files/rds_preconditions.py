478e248c94e0db6e496c908bcc5025bb05af6e6d
lboka <ladislav.boka@t-systems.com>
2024-10-25 20:45:24 +0000

    New storage type fix

--- playbooks/files/rds_preconditions.py
+++ playbooks/files/rds_preconditions.py
@@ -9,64 +9,64 @@ def find_latest_version(datastores):
     max_version = max(datastores, key=lambda x: float(x["name"]))
     return max_version["name"]
 
+def find_smallest_flavor(flavors, az):
+    # Initialize variables to track the smallest flavor
+    smallest_flavor = None
+    smallest_vcpus = float('inf')
+    
+    for flavor in flavors:
+        vcpus = int(flavor['vcpus'])
+        az_status = flavor.get('az_status', {})
+        
+        # Check if the flavor has the desired az
+        if az_status.get(az) == "normal":
+            # Compare the vcpus to find the smallest
+            if vcpus < smallest_vcpus:
+                smallest_vcpus = vcpus
+                smallest_flavor = flavor['spec_code']
+                group_type = flavor['group_type']
+    
+    return smallest_flavor, group_type
+
 
 if __name__ == "__main__":
     try:
-        if len(sys.argv) != 4:
-            print(json.dumps({"error": "check_rds_flavor.py <flavor_code>"}))
+        if len(sys.argv) != 5:
+            print(json.dumps({"error": "Invalid arguments passed"}))
             sys.exit(1)
 
         # Replace placeholders with actual values
         database_name = sys.argv[1]
         db_versions = json.loads(sys.argv[2])
-        spec_code = sys.argv[3]
+        flavors = json.loads(sys.argv[3])
+        az = sys.argv[4]
 
         latest_version = find_latest_version(db_versions)
+        smallest_flavor, group_type = find_smallest_flavor(flavors, az)
 
         # Establish a connection using your OpenStack profile
         conn = openstack.connect()
         sdk.register_otc_extensions(conn)
 
-        # Determine the service type and interface
-        service_type = "rdsv3"  # Adjust this to match your service
-        interface = "public"  # Use 'internal' or 'admin' if necessary
-
-        # Retrieve the service endpoint
-        endpoint = conn.session.get_endpoint(
-            service_type=service_type, interface=interface
-        )
-
         # Retrieve region name
         region_name = conn.config.region_name
 
-        # Construct the URL
-        url1 = (
-            f"{endpoint}/storage-type/{database_name}"
-            f"?version_name={latest_version}"
-        )
-
-        # Make the GET request using the authenticated session
-        response = conn.session.get(url1)
-        data1 = response.json()
-
-        flavors = conn.rdsv3.flavors(
-            datastore_name=database_name,
-            version_name=latest_version,
-            spec_code=spec_code,
-        )
-        flavor_list = list(flavors)
-        group_type = flavor_list[0].group_type
+        storage_types = list(conn.rdsv3.storage_types(
+            datastore_name=database_name, 
+            version_name=latest_version
+            ))
 
         # Initialize storage_type variable
         storage_type = None
 
         # Iterate over storage types in data1
-        for storage in data1["storage_type"]:
+        for storage in storage_types:
             if group_type in storage["support_compute_group_type"]:
                 storage_type = storage["name"]
                 if storage_type == "COMMON":
                     continue
                 break  # Exit the loop after finding the first match
+
 
         # Output the result as JSON
         print(
@@ -75,9 +75,12 @@ if __name__ == "__main__":
                     "region": region_name,
                     "storage_type": storage_type,
                     "latest_version": latest_version,
+                    "smallest_flavor": smallest_flavor,
                 }
             )
         )
 
     except Exception as e:
-        print(f"Error: {str(e)}")
+        # Always return JSON even in case of errors
+        print(json.dumps({"error": str(e)}))
+        sys.exit(1)
