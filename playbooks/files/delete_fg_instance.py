3a38a5b7aa9f3d8e9c3b7bc0f3100b77a43b5a2e
Peter ERDOSI (Fazy) <peter.erdosi@t-systems.com>
2025-07-23 09:41:56 +0000

    repair E111 and E203 errors

--- playbooks/files/delete_fg_instance.py
+++ playbooks/files/delete_fg_instance.py
@@ -9,13 +9,13 @@ instance_name = sys.argv[1]
 
 for fgs in conn.function_graph.functions():
     if fgs["func_name"] == instance_name:
-      urn_for_deletion = fgs["func_urn"]
+        urn_for_deletion = fgs["func_urn"]
 
 
 print(urn_for_deletion)
 
 attrs = {
-    'func_urn': urn_for_deletion + ":" + instance_name ,
+    'func_urn': urn_for_deletion + ":" + instance_name,
 }
 
 fg = conn.functiongraph.delete_function(function=attrs)
