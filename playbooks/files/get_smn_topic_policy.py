f0edfa9606a15b5ef387ed03dad7f9a9487589f3
A86700324 <daniel.kovac@t-systems.com>
2026-02-19 08:42:44 +0000

    more fixes

--- playbooks/files/get_smn_topic_policy.py
+++ playbooks/files/get_smn_topic_policy.py
@@ -5,7 +5,6 @@ import openstack
 import sys
 from otcextensions import sdk
 
-openstack.enable_logging(True)
 conn = openstack.connect()
 
 sdk.register_otc_extensions(conn)
--- playbooks/files/list_smn_topics.py
+++ playbooks/files/list_smn_topics.py
@@ -4,7 +4,6 @@ import openstack
 
 from otcextensions import sdk
 
-openstack.enable_logging(True)
 conn = openstack.connect()
 
 sdk.register_otc_extensions(conn)
