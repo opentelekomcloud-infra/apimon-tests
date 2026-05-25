3c4188cb5fed62a863dcfb6014b2c6693d669709
vladimirhasko <vladimirhasko@gmail.com>
2026-05-25 08:12:44 +0000

    adding proper refernce to otcextensions in lts scripts

--- playbooks/files/delete_lts_loggroup.py
+++ playbooks/files/delete_lts_loggroup.py
@@ -2,7 +2,10 @@
 
 import openstack
 import sys
+from otcextensions import sdk
 
 conn = openstack.connect()
+sdk.register_otc_extensions(conn)
+
 lg_id = sys.argv[1]
 log_group = conn.lts.delete_group(group=lg_id)
--- playbooks/files/update_lts_loggroup.py
+++ playbooks/files/update_lts_loggroup.py
@@ -2,8 +2,10 @@
 
 import openstack
 import sys
+from otcextensions import sdk
 
 conn = openstack.connect()
+sdk.register_otc_extensions(conn)
 
 lg_id = sys.argv[1]
 
